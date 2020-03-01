2a6b6b0b49b100b2f2755fa730568df1e259822f
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-03-01 09:29:57 -0300

    desenvolvimento popup config

--- src/pages/profile/config.js
+++ src/pages/profile/config.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import {useSelector} from "react-redux";
 
 function Config(){
+
+	const { active } = useSelector(store => store.classActive);
+
 	return(
-		<div>config</div>
+		<div id="wrap_config" className={active ? 'active' : '' }>
+			<div className="indent">
+				<div className="item">
+					<span>Alterar senha</span>
+				</div>
+				<div className="item">
+					<span>Sair</span>
+				</div>
+				<div className="item">
+					<span>Cancelar</span>
+				</div>
+			</div>
+		</div>
 	)
 }
 
