76132fd48d402ee17d77e5d2f048ed90f288ab35
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-02-29 21:16:05 -0300

    começo do desenvolvimento do header

--- frontend/src/components/header/index.js
+++ frontend/src/components/header/index.js
@@ -1,5 +1,10 @@
 import React, {useState} from 'react';
-import {LOGIN} from '../../routes';
+import {LOGIN, HOME} from '../../routes';
+import LogoSmall from 'assets/images/logo-small.png';
+import SearchIcon from '@material-ui/icons/Search';
+import CancelIcon from '@material-ui/icons/Cancel';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import HomeIcon from '@material-ui/icons/Home';
 
 function Header({location, handleLogout}){
 
@@ -14,7 +19,21 @@ function Header({location, handleLogout}){
 	return(
 		<>
 		{!hidden && (
-			<div>header
+			<div id="wrap_header">
+				<div className="indent">
+					<div className="logo">
+						<img src={LogoSmall} alt="Instagram"/>
+					</div>
+					<div className="search">
+						<SearchIcon />
+						<input type="text" placeholder="Pesquisar" />
+						<CancelIcon />
+					</div>
+					<div className="menu">
+						<HomeIcon className={location.pathname === HOME ? 'active' : ''} />
+						<AccountCircleIcon />
+					</div>
+				</div>
 				<li onClick={handleLogout}>Logout</li>
 			</div>
 		)}
