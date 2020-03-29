e0e64aa46de74ccf1b8dd6b8a07b206aa12bc575
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-03-29 18:52:23 -0300

    fazendo o redirect caso o usuario esteja logado ou não

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, {lazy, Suspense} from 'react'
-import {Route, Switch, useHistory} from 'react-router-dom';
-import {EDIT, LOGIN, PROFILE} from './routes';
+import React, { lazy, Suspense, useEffect } from 'react'
+import { Route, Switch, useHistory } from 'react-router-dom';
+import { EDIT, LOGIN, PROFILE } from './routes';
 import Header from 'components/header';
 import Footer from 'components/footer';
 import LinearProgress from '@material-ui/core/LinearProgress';
-import {useSelector} from "react-redux";
+import { useSelector } from "react-redux";
 
 const Home = lazy(() => import('pages/index'));
 const Login = lazy(() => import('pages/login'));
@@ -12,18 +12,26 @@ const Profile = lazy(() => import('pages/profile'));
 const Edit = lazy(() => import('pages/edit'));
 
 
-function App({location}){
+function App({ location }){
 
 	const { id } = useSelector(store => store.auth);
 
 	const idLocalStorage = localStorage.getItem('id');
 	const history = useHistory();
 
-	if(id || idLocalStorage){
-		if(location.pathname === LOGIN){
-			history.push('/');
+	useEffect(() => {
+		if(id || idLocalStorage){
+			if(location.pathname === LOGIN){
+				history.push('/');
+			}
 		}
-	}
+		if(!idLocalStorage){
+			if(location.pathname !== LOGIN){
+				history.push('/login');
+			}
+		}
+	}, [id, idLocalStorage, history, location.pathname]);
+
 
 	return(
 		<>
--- src/root.js
+++ src/root.js
@@ -1,20 +1,17 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import App from './App';
 import { Provider } from 'react-redux';
-import { CookiesProvider } from 'react-cookie';
 
 import configRedux from './redux/configRedux';
 const store = configRedux();
 
 const Root = () => (
-	<CookiesProvider>
-		<Provider store={store}>
-			<BrowserRouter>
-				<Route component={App}/>
-			</BrowserRouter>
-		</Provider>
-	</CookiesProvider>
+	<Provider store={store}>
+		<BrowserRouter>
+			<Route component={App}/>
+		</BrowserRouter>
+	</Provider>
 );
 
 export default Root;
