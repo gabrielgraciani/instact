57b6ccba60e12a53035a819480d218d96437cd6f
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-04-19 13:54:36 -0300

    desenvolvimento da pagina de single post

--- src/pages/singlePost/index.js
+++ src/pages/singlePost/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PostTeste from 'assets/images/post_teste.jpg';
 
 const SinglePost = ({ match }) => {
 
@@ -7,7 +8,55 @@ const SinglePost = ({ match }) => {
 	console.log('id', id);
 
 	return (
-		<div>oi</div>
+		<div id="wrap_principal">
+			<div className="indent">
+				<div className="posts single">
+					<div className="post">
+						<div className="body">
+							<img src={PostTeste} alt=""/>
+						</div>
+						<div className="box">
+							<div className="head">
+								<div className="user">
+									<img src={PostTeste} alt=""/>
+									<span>asodihjasdo</span>
+								</div>
+							</div>
+
+							<div className="comments">
+								<div className="item">
+									<div className="image">
+										<img src={PostTeste} alt="" />
+									</div>
+									<div className="content">
+										<span><strong>asdih</strong> sadasdasdsa</span>
+										<span className="hora">2h</span>
+									</div>
+								</div>
+								<div className="item">
+									<div className="image">
+										<img src={PostTeste} alt="" />
+									</div>
+									<div className="content">
+										<span><strong>asdih</strong> sadasdasdsa</span>
+										<span className="hora">2h</span>
+									</div>
+								</div>
+								<div className="item">
+									<div className="image">
+										<img src={PostTeste} alt="" />
+									</div>
+									<div className="content">
+										<span><strong>asdih</strong> sadasdasdsa</span>
+										<span className="hora">2h</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	)
 };
 
