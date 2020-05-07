ed980f55b1f8ed31078fbb7fdb7480c2ec7df2b7
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-05-07 20:41:58 -0300

    desenvolvimento da estilização do chat

--- src/pages/direct/index.js
+++ src/pages/direct/index.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from 'react';
 import CreateIcon from '@material-ui/icons/Create';
 import PostTeste from 'assets/images/post_teste.jpg';
 import DirectImage from 'assets/images/direct.png';
+import PhotoAlbumIcon from '@material-ui/icons/PhotoAlbum';
 
 const Direct = () => {
 
 	const [chatActive, setChatActive] = useState(false);
+	const [value, setValue] = useState('');
 
 	const handleChangeChat = () => {
 		setChatActive(true);
@@ -26,7 +28,7 @@ const Direct = () => {
 					</div>
 
 					<div className="conversas">
-						<div className="conversa" onClick={handleChangeChat}>
+						<div className="conversa item-default" onClick={handleChangeChat}>
 							<div className="image">
 								<img src={PostTeste} alt="AS" />
 							</div>
@@ -35,7 +37,7 @@ const Direct = () => {
 								<span>Online há 1h</span>
 							</div>
 						</div>
-						<div className="conversa">
+						<div className="conversa item-default">
 							<div className="image">
 								<img src={PostTeste} alt="AS" />
 							</div>
@@ -49,7 +51,36 @@ const Direct = () => {
 
 				<div className="box-chat">
 					{chatActive ? (
-						<div>oi</div>
+						<>
+							<div className="item-default">
+								<div className="image">
+									<img src={PostTeste} alt="AS" />
+								</div>
+								<div className="dados">
+									<span className="username">username</span>
+									<span>Online há 1h</span>
+								</div>
+							</div>
+
+							<div className="chat">
+								<div className="message self">
+									<span>minha mensagem</span>
+								</div>
+								<div className="message other">
+									<span>Mensagem de outro</span>
+								</div>
+							</div>
+
+							<form action="">
+								<textarea placeholder="Mensagem..." onChange={(e) => setValue(e.target.value)} />
+								{value === '' ? (
+									<PhotoAlbumIcon />
+								) : (
+									<input type="submit" value="Enviar" />
+								)}
+							</form>
+
+						</>
 					) : (
 						<div className="no-message">
 							<img src={DirectImage} alt="Direct"/>
